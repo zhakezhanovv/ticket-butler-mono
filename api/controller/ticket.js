29f94f09f9d4c7abf8aba9ebb1996b0adb5c5036
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-06-05 23:59:45 +0600

    feat(api): add get-many method to ticket-controller

--- api/controller/ticket.js
+++ api/controller/ticket.js
@@ -4,6 +4,10 @@ class TicketController {
 	static async addOne(req, res) {
 		await TicketService.addOne(req, res);
 	}
+
+	static async getMany(req, res) {
+		await TicketService.getMany(req, res);
+	}
 }
 
 module.exports = TicketController;
