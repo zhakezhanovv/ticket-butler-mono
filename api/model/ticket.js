ec2f914456a74b270ae32b1ecce4c6e0bd9842ce
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-06-05 23:53:36 +0600

    feat(api): add get-many and add-many methods to ticket-model

--- api/model/ticket.js
+++ api/model/ticket.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
 class Ticket {
+	static #dataPath = "data.json";
+	static #dataType = "utf8";
+
 	constructor(title, fromNumber, description, filePath, createdAt) {
 		this.createdAt = new Date(Date.now()).toISOString();
 		this.tel = fromNumber;
@@ -13,6 +17,41 @@ class Ticket {
 		}
 
 		return true;
+	}
+
+	static getMany() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(this.#dataPath, this.#dataType, (err, data) => {
+				if (err) {
+					reject(err);
+				}
+
+				try {
+					const tickets = JSON.parse(data);
+					resolve(tickets);
+				} catch (err) {
+					reject(err);
+				}
+			});
+		});
+	}
+
+	static addMany(tickets) {
+		return new Promise((resolve, reject) => {
+			try {
+				const ticketsJson = JSON.stringify(tickets);
+
+				fs.writeFile(this.#dataPath, ticketsJson, (err) => {
+					if (err) {
+						reject(err);
+					}
+
+					resolve();
+				});
+			} catch (err) {
+				reject(err);
+			}
+		});
 	}
 }
 
