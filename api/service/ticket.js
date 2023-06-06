1e127aa58d0e8d809b925b478febc063ac7f833e
zhakezhanovv <97029797+zhakezhanovv@users.noreply.github.com>
2023-06-06 10:27:51 +0800

    refactor: remove console.log

--- api/service/ticket.js
+++ api/service/ticket.js
@@ -2,38 +2,42 @@ const fs = require("fs");
 const Ticket = require("../model/ticket");
 
 class TicketService {
-	static async addOne(req, res) {
-		const file = req.file;
-		const filePath = file?.path;
-		const { title, fromNumber, description } = req.body;
-		console.log(title, fromNumber, description, file);
+  static async addOne(req, res) {
+    const file = req.file;
+    const filePath = file?.path;
+    const { title, fromNumber, description } = req.body;
 
-		const ticketIsValid = Ticket.isValid(title, fromNumber, description, filePath);
-		if (!ticketIsValid) {
-			res.status(400).send({ message: "Invalid Ticket" });
-			return;
-		}
+    const ticketIsValid = Ticket.isValid(
+      title,
+      fromNumber,
+      description,
+      filePath
+    );
+    if (!ticketIsValid) {
+      res.status(400).send({ message: "Invalid Ticket" });
+      return;
+    }
 
-		const ticket = new Ticket(title, fromNumber, description, filePath);
+    const ticket = new Ticket(title, fromNumber, description, filePath);
 
-		try {
-			const tickets = await Ticket.getMany();
+    try {
+      const tickets = await Ticket.getMany();
 
-			tickets.push(ticket);
-			console.log(tickets);
+      tickets.push(ticket);
+      console.log(tickets);
 
-			await Ticket.addMany(tickets);
+      await Ticket.addMany(tickets);
 
-			res.status(200).send({ message: "Ticket saved" });
-		} catch (err) {
-			res.status(500).send({ message: "Ticket save failed" });
-		}
-	}
+      res.status(200).send({ message: "Ticket saved" });
+    } catch (err) {
+      res.status(500).send({ message: "Ticket save failed" });
+    }
+  }
 
-	static async getMany(req, res) {
-		const tickets = await Ticket.getMany();
-		res.status(200).send({ data: tickets });
-	}
+  static async getMany(req, res) {
+    const tickets = await Ticket.getMany();
+    res.status(200).send({ data: tickets });
+  }
 }
 
 module.exports = TicketService;
--- bot/ticket.js
+++ bot/ticket.js
@@ -1,26 +1,25 @@
 const { postTicket } = require("./api");
 
 async function ticketHandler(client, msg) {
-	const content = msg.body;
-	const contentSplit = content.split("\n");
-	const contact = await msg.getContact();
-	console.log(content);
+  const content = msg.body;
+  const contentSplit = content.split("\n");
+  const contact = await msg.getContact();
 
-	const title = contentSplit[0];
-	const fromNumber = contact.number;
-	const description = contentSplit[1];
-	const file = await msg.downloadMedia();
+  const title = contentSplit[0];
+  const fromNumber = contact.number;
+  const description = contentSplit[1];
+  const file = await msg.downloadMedia();
 
-	try {
-		const response = await postTicket(title, fromNumber, description, file);
+  try {
+    const response = await postTicket(title, fromNumber, description, file);
 
-		client.sendMessage(msg.from, response.message);
-	} catch (err) {
-		console.log(err);
-		client.sendMessage(msg.from, err.message);
-	}
+    client.sendMessage(msg.from, response.message);
+  } catch (err) {
+    console.log(err);
+    client.sendMessage(msg.from, err.message);
+  }
 }
 
 module.exports = {
-	ticketHandler,
+  ticketHandler,
 };
