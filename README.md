7a2d5388df46dea23e29cc9cccce15386edb9c37
zhakezhanovv <97029797+zhakezhanovv@users.noreply.github.com>
2023-06-06 11:07:04 +0800

    feat(readme): impove README file

--- README.md
+++ README.md
@@ -1,29 +1,86 @@
 # ticket-butler-mono
 
-## Установить зависимости
+## Install dependencies
 
 ```sh
 npm install
 ```
 
-## Запустить Whatsapp бота
+## Launch the WhatsApp bot
 
 ```sh
 npm run prod:bot
 ```
 
-## Запустить Api сервер
+## Launch the API server
 
-- Если же вы хотите запустить локально используйте:
+- To launch the API server locally, use:
+
 ```sh
 npm run prod:api
 ```
 
-- Если же вы хотите запустить в контейнере используйте:
+- To launch the API server in a container, use:
+
 ```sh
 docker-compose up -d
 ```
 
-Для того что бы бот работал отсканируйте QRCODE в терминале через Whatsapp-QRCODE, где вы запускали бота
+## API
 
-Далее после того как вам отправят сообщения данные сохранятся в data.json в локальном окружений вашего приложения
+```sh
+ localhost:8080/api/tickets
+```
+
+- To get the whole list of tickets, use GET method.
+
+- To insert a ticket to the data.json file, use PUT method and input into a form-data: title, fromNumber, description(optional) and file(optional).
+
+## How to use the service
+
+1. After launching both the API server and the bot, scan the QR via WhatsApp mobile in the terminal where you launched the bot.
+
+2. Receive messages to the account that the QR code has scanned from. (Somebody has to text a person who scanned the QR code).
+
+3. Test the service with the following methods:
+
+- Send a single text message to the account. Expected output will be:
+
+```json
+[
+  {
+    "createdAt": "2023-06-06T02:20:17.054Z",
+    "tel": "77057017526",
+    "msg": "Не хочу брать😂"
+  }
+]
+```
+
+- Send a message to the account with 2 paragraphs (Title is the 1st line, description is the 2nd line). Expected output will be:
+
+```json
+[
+  {
+    "createdAt": "2023-06-06T02:20:17.054Z",
+    "tel": "77057017526",
+    "msg": "Не хочу брать😂",
+    "description": "Test description"
+  }
+]
+```
+
+- Send a message to the account with 2 paragraphs (Title is the 1st line, description is the 2nd line) and attach a document. The upload files are stored in /uploads. Expected output will be:
+
+```json
+[
+  {
+    "createdAt": "2023-06-06T02:44:59.367Z",
+    "tel": "85268576166",
+    "msg": "Tyf",
+    "description": "Test description",
+    "fileUrl": "uploads/Ali Zhakezhanov.pdf"
+  }
+]
+```
+
+- Send a message with no text. Expected output will be: "Invaid ticket".
--- api/model/ticket.js
+++ api/model/ticket.js
@@ -1,58 +1,58 @@
 const fs = require("fs");
 class Ticket {
-	static #dataPath = "data.json";
-	static #dataType = "utf8";
+  static #dataPath = "data.json";
+  static #dataType = "utf8";
 
-	constructor(title, fromNumber, description, filePath, createdAt) {
-		this.createdAt = new Date(Date.now()).toISOString();
-		this.tel = fromNumber;
-		this.msg = title;
-		this.description = description;
-		this.fileUrl = filePath;
-	}
+  constructor(title, fromNumber, description, filePath, createdAt) {
+    this.createdAt = new Date(Date.now()).toISOString();
+    this.tel = fromNumber;
+    this.msg = title;
+    this.description = description;
+    this.fileUrl = filePath;
+  }
 
-	static isValid(title, fromNumber, description, filePath) {
-		if (!title || title == "" || !fromNumber || fromNumber == "") {
-			return false;
-		}
+  static isValid(title, fromNumber, description, filePath) {
+    if (!title || title == "" || !fromNumber || fromNumber == "") {
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	static getMany() {
-		return new Promise((resolve, reject) => {
-			fs.readFile(this.#dataPath, this.#dataType, (err, data) => {
-				if (err) {
-					reject(err);
-				}
+  static getMany() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(this.#dataPath, this.#dataType, (err, data) => {
+        if (err) {
+          reject(err);
+        }
 
-				try {
-					const tickets = JSON.parse(data);
-					resolve(tickets);
-				} catch (err) {
-					reject(err);
-				}
-			});
-		});
-	}
+        try {
+          const tickets = JSON.parse(data);
+          resolve(tickets);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  }
 
-	static addMany(tickets) {
-		return new Promise((resolve, reject) => {
-			try {
-				const ticketsJson = JSON.stringify(tickets);
+  static addMany(tickets) {
+    return new Promise((resolve, reject) => {
+      try {
+        const ticketsJson = JSON.stringify(tickets);
 
-				fs.writeFile(this.#dataPath, ticketsJson, (err) => {
-					if (err) {
-						reject(err);
-					}
+        fs.writeFile(this.#dataPath, ticketsJson, (err) => {
+          if (err) {
+            reject(err);
+          }
 
-					resolve();
-				});
-			} catch (err) {
-				reject(err);
-			}
-		});
-	}
+          resolve();
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 }
 
 module.exports = Ticket;
