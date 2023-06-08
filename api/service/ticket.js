c7f4d97734f776744db2d4cb3fc9bc919b287177
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-06-08 16:21:23 +0600

    fix: remove unnecessary status, change ticket-is-valid vars

--- api/service/ticket.js
+++ api/service/ticket.js
@@ -2,42 +2,30 @@ const fs = require("fs");
 const Ticket = require("../model/ticket");
 
 class TicketService {
-  static async addOne(req, res) {
-    const file = req.file;
-    const filePath = file?.path;
-    const { title, fromNumber, description } = req.body;
+	static async addOne(req, res) {
+		const file = req.file;
+		const { title, fromNumber, description } = req.body;
 
-    const ticketIsValid = Ticket.isValid(
-      title,
-      fromNumber,
-      description,
-      filePath
-    );
-    if (!ticketIsValid) {
-      res.status(400).send({ message: "Invalid Ticket" });
-      return;
-    }
+		const { isValid, message } = Ticket.isValid(title, fromNumber, description, file);
+		if (!isValid) {
+			res.send({ message });
+			return;
+		}
 
-    const ticket = new Ticket(title, fromNumber, description, filePath);
+		const ticket = new Ticket(title, fromNumber, description, file?.path);
 
-    try {
-      const tickets = await Ticket.getMany();
+		try {
+			await Ticket.addOne(ticket);
+			res.send({ message: "Ticket saved" });
+		} catch (err) {
+			res.send({ message: "Ticket save failed" });
+		}
+	}
 
-      tickets.push(ticket);
-      console.log(tickets);
-
-      await Ticket.addMany(tickets);
-
-      res.status(200).send({ message: "Ticket saved" });
-    } catch (err) {
-      res.status(500).send({ message: "Ticket save failed" });
-    }
-  }
-
-  static async getMany(req, res) {
-    const tickets = await Ticket.getMany();
-    res.status(200).send({ data: tickets });
-  }
+	static async getMany(req, res) {
+		const tickets = await Ticket.getMany();
+		res.send({ data: tickets });
+	}
 }
 
 module.exports = TicketService;
