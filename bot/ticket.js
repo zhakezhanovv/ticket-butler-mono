b77dff04d12d4aa818c65a91343c8bb1b04db1a9
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-06-08 16:15:29 +0600

    fix(bot): change message-body parser to ticket properties

--- bot/ticket.js
+++ bot/ticket.js
@@ -1,25 +1,36 @@
 const { postTicket } = require("./api");
 
 async function ticketHandler(client, msg) {
-  const content = msg.body;
-  const contentSplit = content.split("\n");
-  const contact = await msg.getContact();
+	const content = msg.body;
+	const contact = await msg.getContact();
 
-  const title = contentSplit[0];
-  const fromNumber = contact.number;
-  const description = contentSplit[1];
-  const file = await msg.downloadMedia();
+	const { command, title, description } = getTicketFromString(content);
+	const fromNumber = contact.number;
+	const file = await msg.downloadMedia();
 
-  try {
-    const response = await postTicket(title, fromNumber, description, file);
+	try {
+		if (command != "ticket") return;
 
-    client.sendMessage(msg.from, response.message);
-  } catch (err) {
-    console.log(err);
-    client.sendMessage(msg.from, err.message);
-  }
+		const response = await postTicket(title, fromNumber, description, file);
+		client.sendMessage(msg.from, response.message);
+	} catch (err) {
+		console.log(err);
+		client.sendMessage(msg.from, err.message);
+	}
+}
+
+function getTicketFromString(str) {
+	const strSplit = str.split("\n");
+	const commandMatch = strSplit[0] ? strSplit[0].match(/#(.+)/) : null;
+	const command = commandMatch ? commandMatch[1].trim() : "";
+	const titleMatch = strSplit[1] ? strSplit[1].match(/Title:\s*(.+)/) : "";
+	const title = titleMatch ? titleMatch[1].trim() : "";
+	const descriptionMatch = strSplit[2] ? strSplit[2].match(/Description:\s*(.+)/) : "";
+	const description = descriptionMatch ? descriptionMatch[1].trim() : "";
+
+	return { command, title, description };
 }
 
 module.exports = {
-  ticketHandler,
+	ticketHandler,
 };
