c29bcd04bd2d3347782334c04a16d8189c60061b
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-06-08 16:20:07 +0600

    fix: add id for ticket, change is-valid-method, add add-one method for ticket-entitie

--- api/model/ticket.js
+++ api/model/ticket.js
@@ -1,58 +1,103 @@
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
 class Ticket {
-  static #dataPath = "data.json";
-  static #dataType = "utf8";
+	static #dataPath = "data.json";
+	static #dataType = "utf8";
 
-  constructor(title, fromNumber, description, filePath, createdAt) {
-    this.createdAt = new Date(Date.now()).toISOString();
-    this.tel = fromNumber;
-    this.msg = title;
-    this.description = description;
-    this.fileUrl = filePath;
-  }
+	constructor(title, fromNumber, description, filePath) {
+		this.id = uuidv4();
+		this.createdAt = new Date(Date.now()).toISOString();
+		this.tel = fromNumber;
+		this.msg = title;
+		if (description != "") {
+			this.description = description;
+		}
+		if (filePath != "") {
+			this.fileUrl = filePath;
+		}
+	}
 
-  static isValid(title, fromNumber, description, filePath) {
-    if (!title || title == "" || !fromNumber || fromNumber == "") {
-      return false;
-    }
+	static isValid(title, fromNumber, description, file) {
+		if (!title || title == "") {
+			return { isValid: false, message: "Invalid title" };
+		}
+		if (!fromNumber || fromNumber == "") {
+			return { isValid: false, message: "Your number is not a valid number" };
+		}
+		if (!!description && description != "" && description.split(" ").length < 2) {
+			return { isValid: false, message: "Invalid description" };
+		}
+		if (!!file && (!file.filename || file.filename == "" || !file.path)) {
+			return { isValid: false, message: "Invalid file" };
+		}
 
-    return true;
-  }
+		return { isValid: true, message: "" };
+	}
 
-  static getMany() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(this.#dataPath, this.#dataType, (err, data) => {
-        if (err) {
-          reject(err);
-        }
+	static getMany() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(this.#dataPath, this.#dataType, (err, data) => {
+				if (err) {
+					reject(err);
+				}
 
-        try {
-          const tickets = JSON.parse(data);
-          resolve(tickets);
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
-  }
+				try {
+					const tickets = JSON.parse(data);
+					resolve(tickets);
+				} catch (err) {
+					reject(err);
+				}
+			});
+		});
+	}
 
-  static addMany(tickets) {
-    return new Promise((resolve, reject) => {
-      try {
-        const ticketsJson = JSON.stringify(tickets);
+	static addMany(tickets) {
+		return new Promise((resolve, reject) => {
+			try {
+				const ticketsJson = JSON.stringify(tickets);
 
-        fs.writeFile(this.#dataPath, ticketsJson, (err) => {
-          if (err) {
-            reject(err);
-          }
+				fs.writeFile(this.#dataPath, ticketsJson, (err) => {
+					if (err) {
+						reject(err);
+					}
 
-          resolve();
-        });
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
+					resolve();
+				});
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
+
+	static async addOne(ticket) {
+		try {
+			const tickets = await Ticket.getMany();
+			const findIndex = tickets.findIndex(
+				(item) => ticket.msg == item?.msg && ticket.tel == item?.tel
+			);
+			if (findIndex == -1) {
+				tickets.push(ticket);
+				await Ticket.addMany(tickets);
+
+				return tickets;
+			} else {
+				const findTicket = tickets[findIndex];
+				const updatedAt = new Date(Date.now()).toISOString();
+				tickets[findIndex] = {
+					...findTicket,
+					description: ticket.description,
+					fileUrl: ticket.fileUrl,
+					updatedAt,
+				};
+				await Ticket.addMany(tickets);
+
+				return tickets;
+			}
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 }
 
 module.exports = Ticket;
